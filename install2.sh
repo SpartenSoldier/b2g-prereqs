78da53a88c69a0ca353085149d0b874feb48fe88
SpartenSoldier <steve.everitt1990@gmail.com>
2013-02-09 13:31:58 -0500

    Newest Version

--- install2.sh
+++ install2.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 sudo apt-get install gcc -y
-sudo apt-get install autoconf2.13 bison bzip2 ccache curl flex gawk gcc-4.6 g++-4.6 g++-4.6-multilib git ia32-libs lib32ncurses5-dev lib32z1-dev libgl1-mesa-dev libX11-dev make -y
+sudo apt-get install autoconf2.13 bison bzip2 ccache curl flex gawk gcc-4.6 g++-4.6 g++-4.6-multilib git ia32-libs lib32ncurses5-dev lib32z1-dev libgl1-mesa-dev libX11-dev make
 
-echo "export CC=`which gcc-4.6`" > B2G/.userconfig
-echo "export CXX=`which g++-4.6`" >> B2G/.userconfig
+sudo ln -s /usr/lib/i386-linux-gnu/libX11.so.6 /usr/lib/i386-linux-gnu/libX11.so sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
 ccache --max-size 3GB
 
 git clone git://github.com/mozilla-b2g/B2G.git
 cd B2G/
+echo "export CC=`which gcc-4.6`" > B2G/.userconfig
+echo "export CXX=`which g++-4.6`" >> B2G/.userconfig
 ./config.sh emulator
 
 git pull
