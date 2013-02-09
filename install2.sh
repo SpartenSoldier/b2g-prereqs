5a6c8b6ead842742a9545bbdcb42e6a4d207ba64
SpartenSoldier <steve.everitt1990@gmail.com>
2013-02-08 23:34:26 -0500

    Did things

--- install2.sh
+++ install2.sh
@@ -1,19 +1,10 @@
 #! /bin/bash
 
-# What Linux distro are we on?
-OS= sed -n 's/^NAME=//p' /etc/*-release
-echo $OS
-
 sudo apt-get install gcc -y
-# yum install gcc -y
 sudo apt-get install autoconf2.13 bison bzip2 ccache curl flex gawk gcc-4.6 g++-4.6 g++-4.6-multilib git ia32-libs lib32ncurses5-dev lib32z1-dev libgl1-mesa-dev libX11-dev make -y
 
-sudo ln -s /usr/lib/i386-linux-gnu/libX11.so.6 /usr/lib/i386-linux-gnu/libX11.so<br /><br />$ sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 10
-update-alternatives: using /usr/bin/gcc-4.7 to provide /usr/bin/gcc (gcc) in auto mode -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 5 -y
-sudo update-alternatives --config gcc -y
+echo "export CC=`which gcc-4.6`" > B2G/.userconfig
+echo "export CXX=`which g++-4.6`" >> B2G/.userconfig
 
 ccache --max-size 3GB
 
